d758b6b578355b7b39d83f111fe85caceb1da847
Soumyadeep Chandra <soumyadeepchandra2002@gmail.com>
2022-07-17 14:40:03 +0530

    Added new files

---
+++ Recursion/power.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int power(int x,int n)
+{
+    if(n==0)
+    return 1;
+    if(n==1)
+    return x;
+    return x*power(x,n-1);
+}
+
+int main()
+{
+    cout<<power(7,4);
+}
